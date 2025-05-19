e9e86243ed361980e4b50774997e31ef077a6d2e
MATIAS DANIEL HALAC <48792865@ort>
2025-05-19 08:51:43 -0300

    codigo halac

--- TpcomponentesRN/App.js
+++ TpcomponentesRN/App.js
@@ -9,24 +9,19 @@ export default function App() {
   return (
    
       <View style={styles.container}>
-        <ImageBackground
-          source = {imgFondo}
-          resizeMode="cover"
-          style={styles.background}
-        >
+        <ImageBackground source = {imgFondo} resizeMode="cover" style={styles.background}>
           <StatusBar style="light" />
           <SafeAreaView style={{flex:1}}>
             <View style={styles.cardContainer}>
               <Image source={imgPerfil} style={styles.image}/>
               <Text style={{fontWeight: 'bold', fontSize: 20, fontFamily: 'Georgia'}}>Matias Halac</Text>
               <Text style={{marginTop: '2%'}}>Full Stack Developer</Text>
-              <TextInput keyboardType='default' style={styles.Ingreso}>Nombre</TextInput>
+              <TextInput keyboardType='default' style={styles.Ingreso} placeholder="Nombre"></TextInput>
               <TouchableOpacity onPress={() => Alert.alert("¡Hola!")} style={styles.Touchable}><Text style={{color: 'white', textAlign: 'center'}}>Contactar</Text></TouchableOpacity>
               <Pressable onPress={() => Alert.alert("El perfil esta en contruccion vuelva pronto")} style={styles.Pressable}>
                 <Text style={{textAlign: 'center', color: 'white'}}>Ver Perfil</Text>
               </Pressable>
             </View>
-           
           </SafeAreaView>
         </ImageBackground>
       </View>
@@ -47,33 +42,46 @@ const styles = StyleSheet.create({
     width: '100%'
   },
   cardContainer: {
+    display: 'flex',
     borderRadius: '10%',
     backgroundColor: 'white',
     width: '60%',
-    height: '40%',
+    height: '37%',
     margin: '20%',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: '16px'
   },
   image: {
+    flex: 1,
     margin: '10%',
     width: '60%',
     height: '40%',  
   },
   Ingreso: {
+    borderRadius: '5%',
     textAlign: 'center',
     borderColor: 'black',
     borderWidth: 1,
-    width: '55%',
+    width: '75%',
+    height: '10%',
     margin: '2%'
   },
   Touchable: {
-    width: '55%',
+    borderRadius: '5%',
+    width: '75%',
     backgroundColor: 'black'
   },
   Pressable: {
+    borderRadius: '5%',
     marginTop: '2%',
     textAlign: 'center',
-    width: '55%',
+    width: '75%',
     backgroundColor: 'blue'
+  },
+  SafeAreaView:{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
